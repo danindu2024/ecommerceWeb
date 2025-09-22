32431b7f8a0961b3e106cb37d85a10208352574a
danindu2024 <daninduransika@gmail.com>
2025-09-22 05:49:24 +0530

    remove font family

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/popular'
+import Offers from '../Components/Offers/offers'
 
 const Shop = () => {
   return (
     <div>
-      
+      <Hero />
+      <Popular />
+      <Offers />
     </div>
   )
 }
