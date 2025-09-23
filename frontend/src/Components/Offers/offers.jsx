832fa4f017010e6e7dc635658fba95a6fd1f111e
danindu2024 <daninduransika@gmail.com>
2025-09-23 07:38:54 +0530

    added offers section

--- frontend/src/Components/Offers/offers.jsx
+++ frontend/src/Components/Offers/offers.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import './offers.css'
-import exclusive_img from '../Asserts/exclusive_image.png'
+import Exclusive_img from '../Asserts/exclusive_image.png'
 
 const offers = () => {
   return (
     <div className='offers'>
       <div className="offers-left">
         <h1>Exclusive</h1>
-        <h1>Offers for You</h1>
-        <p>ONLY ON BEST SELERS PRODUCT</p>
+        <h1>Offers For You</h1>
         <button>Check Now</button>
       </div>
       <div className="offers-right">
-        <img src={exclusive_img} alt="" />
+        <img src={Exclusive_img} alt="" />
       </div>
     </div>
   )
