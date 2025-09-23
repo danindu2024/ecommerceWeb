5125cdb606686c6eb2bc536987c9c0ee717f6a8d
danindu2024 <daninduransika@gmail.com>
2025-09-23 13:32:45 +0530

    display products

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom'
+import Breadcrumb from '../Components/BreadCrumb/Breadcrumb'
+import ProductDispaly from '../Components/ProductDisplay/ProductDispaly'
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext)
+  const {productId} = useParams()
+  const product = all_product.find(product => product.id === Number(productId))
   return (
     <div>
-      
+      <Breadcrumb product={product} />
+      <ProductDispaly product={product} />
     </div>
   )
 }
