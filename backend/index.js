01f6e4535b85abdf324ef741162e8877f69094f2
danindu2024 <daninduransika@gmail.com>
2025-09-24 10:04:40 +0530

    newly added products

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const multer = require('multer')
 const path = require('path')
-const cors = require('cors')
+const cors = require('cors');
+const { type } = require('os');
 
 app.use(express.json())
 app.use(cors())
@@ -135,6 +136,63 @@ app.get('/allProducts', async (req, res) => {
     res.send(products)
     
 })
+
+// User schema
+
+const Users = mongoose.model('Users', {
+    name: {
+        type: String,
+    },
+    emai: {
+        type: String,
+        unique: true,
+    },
+    password:{
+        type: String
+    },
+    cartData: {
+        type: Object
+    },
+    date: {
+        type: Date,
+        default:Date.now,
+    }
+})
+
+// user registration endpoint
+
+app.post('/signup', async (req, res) => {
+    let check = await Users.findOne({email: req.body.email})
+
+    if(check){
+        return res.status(400).json({success:false, error:"exisitng user foundfor the given email"})
+
+    }
+    let cart = {};
+
+    for(let i=0; i < 300; i++){
+        cart[i] = 0
+    }
+
+    const user = new Users({
+        name: req.body.name,
+        email:req.body.emai,
+        password:req.body.password,
+        cartData:cart
+    })
+
+    await user.save()
+
+    const data = {
+        user:{
+            id:user.id
+        }
+    }
+
+    const token = jwt.sign(data, 'secret_ecom');
+    res.json({success: true, token})
+    
+})  
 
 app.listen(port, (error)=>{
     if(!error){
