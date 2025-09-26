c9c4247451ed16fd6086ede67f4ca40e318a3cbd
danindu2024 <daninduransika@gmail.com>
2025-09-26 10:38:54 +0530

    Made breadcrumbs clickable

--- frontend/src/Components/BreadCrumb/Breadcrumb.jsx
+++ frontend/src/Components/BreadCrumb/Breadcrumb.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import './Breadcrumb.css'
-import arrow_icon from '../Asserts/breadcrum_arrow.png'
+import React from 'react';
+import './Breadcrumb.css';
+import arrow_icon from '../Asserts/breadcrum_arrow.png';
+import { Link } from 'react-router-dom'; // Optional if you want clickable breadcrumbs
 
-const Breadcrumb = (props) => {
-    const {product} = props
+const Breadcrumb = ({ product }) => {
+  if (!product) return null; // Handle missing product
+
   return (
     <div className='breadcrumb'>
-      home <img src={arrow_icon} alt="" /> shop <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
+      <Link to="/">Home</Link>
+      <img src={arrow_icon} alt="arrow" />
+      
+      <Link to={`/${product.category.toLowerCase()}`}>{product.category}</Link>
+      <img src={arrow_icon} alt="arrow" />
+      
+      <span>{product.name}</span>
     </div>
-  )
-}
+  );
+};
 
-export default Breadcrumb
+export default Breadcrumb;
